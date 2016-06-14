486f3d43f5a1b1444dbcfbe171a3a8f5568c65a7
Angad Dubey <angad_dubey@Hotmail.com>
2016-06-13 20:43:53 -0400

    stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Funnel Job Boards</div>
             </div>
         </div>
     </body>
